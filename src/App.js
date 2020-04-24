06915dc9a8bc416c9c371fb15d4378a1e286b00c
Jeffery Zhang <zhenyu051988@gmail.com>
2020-04-24 00:19:52 -0700

    init the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
