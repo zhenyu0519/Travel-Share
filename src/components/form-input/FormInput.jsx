23cf67ed45876330adbe6804f498a5250220986b
Jeffery Zhang <zhenyu051988@gmail.com>
2020-04-26 13:16:26 -0700

    add validator

--- src/components/form-input/FormInput.jsx
+++ src/components/form-input/FormInput.jsx
@@ -1,25 +1,52 @@
 import React, { useReducer } from "react";
 import "./FormInput.css";
+import { validate } from "../../utils/validator";
 const inputReducer = (state, action) => {
   switch (action.type) {
     case "CHANGE":
       return {
         ...state,
         value: action.val,
-        isValid: true,
+        isValid: validate(action.val, action.validators),
+      };
+    case "TOUCH":
+      return {
+        ...state,
+        isTouched: true,
       };
     default:
       return state;
   }
 };
-export const FormInput = ({ id, label, element, type, placeholder, rows, errorText }) => {
+export const FormInput = ({
+  id,
+  label,
+  element,
+  type,
+  placeholder,
+  rows,
+  errorText,
+  validators,
+}) => {
   const [inputState, dispatch] = useReducer(inputReducer, {
     value: "",
     isValid: false,
+    isTouched: false,
   });
   const changeHandler = (event) => {
-    dispatch({ type: "CHANGE", val: event.target.value });
+    dispatch({
+      type: "CHANGE",
+      val: event.target.value,
+      validators: validators,
+    });
   };
+
+  const touchHandler = () => {
+    dispatch({
+      type: "TOUCH",
+    });
+  };
+
   const elementContent =
     element === "input" ? (
       <input
@@ -28,6 +55,7 @@ export const FormInput = ({ id, label, element, type, placeholder, rows, errorTe
         placeholder={placeholder}
         onChange={changeHandler}
         value={inputState.value}
+        onBlur={touchHandler}
       />
     ) : (
       <textarea
@@ -35,17 +63,18 @@ export const FormInput = ({ id, label, element, type, placeholder, rows, errorTe
         rows={rows || 3}
         onChange={changeHandler}
         value={inputState.value}
+        onBlur={touchHandler}
       />
     );
   return (
     <div
       className={`form-control ${
-        !inputState.isValid && "form-control-invalid"
+        !inputState.isValid && inputState.isTouched && "form-control-invalid"
       }`}
     >
       <label htmlFor={id}>{label}</label>
       {elementContent}
-      {!inputState.isValid && <p>{errorText}</p>}
+      {!inputState.isValid && inputState.isTouched && <p>{errorText}</p>}
     </div>
   );
 };
--- src/pages/place-page/PlacePage.jsx
+++ src/pages/place-page/PlacePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./PlacePage.css";
 import { FormInput } from "../../components/form-input/FormInput";
+import { VALIDATOR_REQUIRE } from "../../utils/validator";
 
 const PlacePage = () => {
   return (
@@ -9,7 +10,7 @@ const PlacePage = () => {
         element="input"
         type="input"
         label="title"
-        validators={[]}
+        validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter valid title"
       />
     </form>
